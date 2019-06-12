7008293f176c3dd768878c2d1a9304ab5260d3b9
unknown <alpa.gupta@gmail.com>
2019-06-12 12:24:28 -0400

    initial commit

---
+++ Embedded Board.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EmbeddedSystemsArchitecture
+{
+    /// <summary>
+    /// Interaction logic for Embedded_Board.xaml
+    /// </summary>
+    public partial class Embedded_Board : Window
+    {
+        public Embedded_Board()
+        {
+            InitializeComponent();
+        }
+
+        private void Verilog_Example_Click(object sender, RoutedEventArgs e)
+        {
+            VerilogExample VerilogExampleObject = new VerilogExample();
+            VerilogExampleObject.Show();
+        }
+
+        private void Allegro_Editor_Click(object sender, RoutedEventArgs e)
+        {
+            AllegroEditor AllegroEditorObject = new AllegroEditor();
+            AllegroEditorObject.Show();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmbeddedSystemsArchitecture
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EmbeddedBoard_Click(object sender, RoutedEventArgs e)
+        {
+            Embedded_Board Embedded_BoardObject = new Embedded_Board();
+            Embedded_BoardObject.Show();
+        }
+
+        private void SystemsSoftwareLayer_Click(object sender, RoutedEventArgs e)
+        {
+            SystemsSoftwareLayer SystemsSoftwareLayerObject = new SystemsSoftwareLayer();
+            SystemsSoftwareLayerObject.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ApplicationSoftwareLayer ApplicationSoftwareLayerObject = new ApplicationSoftwareLayer();
+            ApplicationSoftwareLayerObject.Show();
+        }
+
+       
+    }
+}
